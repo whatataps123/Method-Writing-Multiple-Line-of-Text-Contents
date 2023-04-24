36c5d0c5e1806abc5969f26b992dd47468691766
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-04-25 07:26:25 +0800

    pseudocode

--- Writing Multiple Line Contents.py
+++ Writing Multiple Line Contents.py
@@ -1,7 +1,9 @@
 # Joshua Lemuel Z. Centina BS CPE 1-4
 # Writing Multiple Line Contents
 
+def text():
 # Write a method in python to write multiple line of text contents into a text file mylife.txt. See sample output:
+    with open("mylife.txt", "a") as input_text:
 # Enter line: Beautiful is better than ugly.
 # Are there more lines y/n? y
 # Enter line: Explicit is better than implicit.
@@ -9,3 +11,4 @@
 # Enter line: Simple is better than complex.
 # Are there more lines y/n? n 
 
+    text()
