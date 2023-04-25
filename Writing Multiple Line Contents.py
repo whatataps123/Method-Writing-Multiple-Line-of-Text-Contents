52e7363d229906e5c21ea313770487ff6b9c59df
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-04-26 00:05:14 +0800

    Program Working

--- Writing Multiple Line Contents.py
+++ Writing Multiple Line Contents.py
@@ -10,10 +10,15 @@
 # Are there more lines y/n? n 
 
 # pseudocode
+print("=" * 51)
+print("\033[1;32mWelcome to Writing Multiple Lines of Text Contents!\033[0m")
+print("=" * 51)
 #define a function
 def text():
 # open mylife.txt (read) and write mode as text_input
     with open("mylife.txt", "w") as text_input:
+# Welcome to this program
+#"\033[1;xm\033[0m"
 # use loop to execute the block of code repeatedly
         while True:
             line = input("Enter line: ")
@@ -23,11 +28,13 @@ def text():
             if add_line.lower() != "y":
                 break
 # print the output
-    print("Inputted lines: ")
+    print("=" * 51)
+    print("\033[1;36mInputted lines: \033[0m")
     with open("mylife.txt") as text_input:
         #read each line by line
         for line in text_input:
             print(line.strip())
+    print("=" * 51)
 # close the file
 
 text()
